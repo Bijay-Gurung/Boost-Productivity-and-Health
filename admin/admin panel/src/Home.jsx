3e2e601f568e201dd82c48dd38fc19fccb5bf70b
Bijay-Gurung <stha4580@gmail.com>
2025-04-02 23:50:59 +0545

    working on admin home page

--- admin/admin panel/src/Home.jsx
+++ admin/admin panel/src/Home.jsx
@@ -1,10 +1,34 @@
 import React, { useState } from 'react';
 // import {useNavigate} from 'react-router-dom';
 
+function Nav(){
+    return(
+        <>
+        <div className='Nav'>
+            <div className='userProfile'>
+                <div className='adminPhoto'></div>
+                <h3>Bijay Gurung</h3>
+                <p className='Name'>Stha4580@gmail.com</p>
+            </div>
+
+            <h2>Admin Panel</h2>
+        </div>
+        </>
+    );
+}
+
+function SideNav(){
+    return(
+        <>
+        </>
+    );
+}
+
 export default function Home(){
     return(
         <>
-        <h1>HI</h1>
+        <Nav />
+        <SideNav />
         </>
     );
 }
